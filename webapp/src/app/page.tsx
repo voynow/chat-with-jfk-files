fb17618e418b88cbeaa93777ee595351abe723b7
voynow <voynow99@gmail.com>
2025-02-04 20:50:23 -0500

    api url hotfix

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
     setInput("");
 
     try {
-      const response = await fetch(process.env.NEXT_PUBLIC_API_URL_LOCAL!, {
+      const response = await fetch(process.env.NEXT_PUBLIC_API_URL!, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
